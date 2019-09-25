a5dbacec2ae4dc9f8f788fe44e5d9f8bef65d393
杨俊艺 <1209032479@qq.com>
2019-09-25 20:31:50 +0800

    最初版本,完全没有设计模式可言

--- Concentration/ViewController.swift
+++ Concentration/ViewController.swift
@@ -10,11 +10,37 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var flipCount: Int = 0 {
+        didSet{
+            flipCountLabel.text = "Flips: \(flipCount)"
+        }
+    }
+    
+    @IBOutlet weak var flipCountLabel: UILabel!
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func touchCard(_ sender: UIButton) {
+        flipCard(withEmoji: "♑︎", on: sender);
+        flipCount += 1
+    }
+    
+    @IBAction func touchSecondCard(_ sender: UIButton) {
+        flipCard(withEmoji: "♈︎", on: sender);
+        flipCount += 1
+    }
+    
+    func flipCard(withEmoji emoji: String, on button: UIButton) {
+        if button.currentTitle == emoji {
+            button.setTitle("", for: UIControl.State.normal)
+            button.backgroundColor = UIColor.orange
+        }else{
+            button.setTitle(emoji, for: UIControl.State.normal)
+            button.backgroundColor = UIColor.white
+        }
+    }
 }
 
