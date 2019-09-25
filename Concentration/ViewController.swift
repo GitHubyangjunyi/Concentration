a000d6fe4899690bf4b2f50ab8a921a9b1a7e675
杨俊艺 <1209032479@qq.com>
2019-09-25 21:17:50 +0800

    第二版本更新为数据驱动,还没有MVC

--- Concentration/ViewController.swift
+++ Concentration/ViewController.swift
@@ -10,27 +10,25 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var flipCountLabel: UILabel!//指向显示点击次数的标签
+    @IBOutlet var cardButtons: [UIButton]!//集合所有的按钮统一管理
+    
+    var emojiChoices = ["🇺🇸", "🇨🇦", "🇨🇳", "🇩🇪", ]
+    
     var flipCount: Int = 0 {
         didSet{
             flipCountLabel.text = "Flips: \(flipCount)"
         }
     }
     
-    @IBOutlet weak var flipCountLabel: UILabel!
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
-    }
-
     @IBAction func touchCard(_ sender: UIButton) {
-        flipCard(withEmoji: "♑︎", on: sender);
         flipCount += 1
-    }
-    
-    @IBAction func touchSecondCard(_ sender: UIButton) {
-        flipCard(withEmoji: "♈︎", on: sender);
-        flipCount += 1
+        //获得被点击的那个按钮在按钮集合中的下标,返回值是可选类型,如果按钮没有关联到按钮集合进行统一管理则那个按钮并不在按钮集合中,就找不到,返回nil
+        let cardNumber = cardButtons.firstIndex(of: sender)!//-->强制解析可选类型,如果没有则crash//let cardNumber = cardButtons.firstIndex(of: sender)//不采用宽松政策
+        
+        flipCard(withEmoji: emojiChoices[cardNumber], on: sender)//每个按钮对应一个emoji表情
+        
+        //print("cardNumber = \(String(describing: cardNumber))")
     }
     
     func flipCard(withEmoji emoji: String, on button: UIButton) {
@@ -43,4 +41,3 @@ class ViewController: UIViewController {
         }
     }
 }
-
